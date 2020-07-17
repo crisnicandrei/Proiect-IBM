a28d769333325ae61ced795376796d4de0ed83bf
Andrei <crisnicandrei@gmail.com>
2020-07-17 13:47:17 +0300

    Form Login

---
+++ exams/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import FormLogin from './FormLogin';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <FormLogin />
+    </div>
+  );
+}
+
+export default App;
