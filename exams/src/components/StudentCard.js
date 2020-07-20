370d6ac0857fee9978505ef6532a654e09e810ff
Andrei <crisnicandrei@gmail.com>
2020-07-20 15:39:10 +0300

    Added student card

---
+++ exams/src/components/StudentCard.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class StudentCard extends Component {
+    render() {
+        return (
+            <div>
+                Student info will go here!
+            </div>
+        )
+    }
+}
