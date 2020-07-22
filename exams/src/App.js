1d42acfd27df74141f8f9279c98bb65b82bde0b5
Andrei <crisnicandrei@gmail.com>
2020-07-22 13:26:15 +0300

    Create prof form

--- exams/src/App.js
+++ exams/src/App.js
@@ -5,6 +5,7 @@ import { Switch, Route } from 'react-router-dom';
 import PageProfesor from './components/pageProfesor';
 import PageStudent from './components/pageStudent';
 import PageAdmin from './components/pageAdmin';
+import FormProfessor from './components/FormProfessor'
 
 
 function App() {
@@ -12,12 +13,13 @@ function App() {
     <div className="App">
 
 
-      <Switch >
+      {/* <Switch >
         <Route path="/" exact component={FormLogin} />
         <Route path="/prof" exact component={PageProfesor} />
         <Route path="/stud/home" exact component={PageStudent} />
         <Route path="/admin" exact component={PageAdmin} />
-      </Switch>
+      </Switch> */}
+      <FormProfessor />
     </div>
   );
 }
