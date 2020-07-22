9e002beabd30f1b8ca31a46a262a68aec6b11e2e
Andrei <crisnicandrei@gmail.com>
2020-07-22 23:12:20 +0300

    more styling added to the student cards

--- exams/src/components/layouts/NavbarStudent.js
+++ exams/src/components/layouts/NavbarStudent.js
@@ -4,11 +4,13 @@ import { FaBars } from 'react-icons/fa';
 //Import for Link router
 import { Link } from 'react-router-dom';
 // import '..//../css/NavbarStudent.css'
+import {FiLogOut} from 'react-icons/fi'
+import {GoHome} from 'react-icons/go'
 
 function NavbarStudent() {
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+        <nav className="navbar navbar-expand-lg     ">
 
             <Link className="navbar-brand ml-5" to="/"><img className="logo-img" alt="" />Student</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -18,10 +20,10 @@ function NavbarStudent() {
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav ml-auto text-uppercase ">
                     <li className="nav-item active">
-                        <Link className="nav-link text-white ml-5" to="home">Home <span className="sr-only">(current)</span></Link>
+                        <Link className="nav-link text-white ml-5" to="home"><GoHome className='Icon'  /><span className="sr-only">(current)</span></Link>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link text-white ml-5" to="../">Log Out</Link>
+                        <Link className="nav-link text-white ml-5" to="../"><FiLogOut className='Icon' /></Link>
                     </li>
 
                 </ul>
--- exams/src/components/student/StudentCard.js
+++ exams/src/components/student/StudentCard.js
@@ -4,10 +4,12 @@ import '../../css/StudentCard.css'
 function StudentCard(props) {
 
     return (
-        <Col className='StudentCard block-example '  >
+        <Col className='StudentCard '  >
 
             <h4>Exam {props.exam} </h4>
+            <hr></hr>
             <h4>Date {props.data}</h4>
+            <hr></hr>
             <h4>Professor {props.profesor}</h4>
 
         </Col>
