370c5f24d5dace254e3b4816760ed228f3bcb7dc
Abiam DRONCA <abiamdronca@yahoo.com>
2020-07-31 12:19:28 +0300

    Backend new update

--- Backend/src/main/java/com/example/persistance/repository/PlanningRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.persistance.repository;
-
-import com.example.persistance.entity.Planning;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlanningRepository extends JpaRepository<Planning, Long> {
-}
--- Backend/src/main/java/com/example/persistance/repository/TeacherRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.persistance.repository;
-
-import com.example.persistance.entity.Teacher;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeacherRepository extends JpaRepository<Teacher, Long> {
-}
