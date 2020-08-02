5d8f4fd2797c0b7037ac977583c2aace51588dc7
RusuVlad99 <vlad.rusu@student.upt.ro>
2020-08-02 15:53:22 +0300

    Added CrossOrigin for all controllers on port 3000

--- Backend/src/main/java/com/example/controller/LoginController.java
+++ Backend/src/main/java/com/example/controller/LoginController.java
@@ -3,13 +3,11 @@ package com.example.controller;
 import com.example.persistance.entity.Login;
 import com.example.service.function.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class LoginController {
 
--- Backend/src/main/java/com/example/controller/StudentsController.java
+++ Backend/src/main/java/com/example/controller/StudentsController.java
@@ -3,12 +3,14 @@ package com.example.controller;
 import com.example.persistance.entity.Exam;
 import com.example.service.function.StudentsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class StudentsController {
 
